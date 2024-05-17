82562b1bca93f229b2e6748e7e99ebe37e84762e
kantaro36 <22fi021@ms.dendai.ac.jp>
2024-05-17 09:10:48 +0900

    ntpro第4課題

---
+++ socket-1/src/Renshu.java
@@ -0,0 +1,16 @@
+public class Renshu {
+    // xを2倍にして返す関数
+    public int doubleValue(int x) {
+        return x * 2;
+    }
+
+    //ここに続きを実装していく。
+
+    public int sumUpToN(int x) {
+        return (x / 2) * (x + 1);
+    }
+
+    public int sumFromPtoQ(int x, int y){
+        return (y / 2) * (x + y);
+    }
+}
